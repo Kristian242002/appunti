2faab89d94974d6dfcd711bc17f496c348bbb64e
Kristian242002 <kristianxhani20@gmail.com>
2026-08-08 16:15:03 +0200

    caricato il tutto

---
+++ università/Sicurezza delle Reti/DalWebAlWebServices/WebSocket_Chat/public/chat.js
@@ -0,0 +1,44 @@
+
+//Client
+
+var name= prompt("What's your name?");
+while(name==""){
+    name=prompt("You have to choose a name. \n What's your name?")
+}
+
+// Query DOM
+var message = document.getElementById('message'),
+   sender = document.getElementById('sender'),
+   btn = document.getElementById('send'),
+   output = document.getElementById('output'),
+   feedback = document.getElementById('feedback');
+
+sender.innerHTML=name;  
+sender.value=name;
+
+// Invio richiesta di connessione al server
+var webSocket = io.connect();
+
+// Listen for events
+btn.addEventListener('click', function(){
+   if (message.value!="" ){    
+ 	   webSocket.emit('messaggio', {
+        message: message.value,
+ 	   sender: sender.value,
+ 	   });
+ 	   message.value = "";
+   }
+});
+
+webSocket.on('aggiornamento', function(data){
+   feedback.innerHTML = '';
+   output.innerHTML += '<p><strong>' + data.sender + ': </strong>' + data.message + '</p>';
+});
+
+// Esercizio 5
+message.addEventListener("keydown",function(){
+    webSocket.emit('typing', { sender: sender.value }); // catturo i tasti che vengono prmuti da tastiere e lo invio al server
+});
+webSocket.on('typing', function(data){
+   console.log(data.sender + ' sta scrivendo...');
+});
