d6f38dbfc058dc2e2eb9aaa298013d8bf75cc15a
Kristian242002 <kristianxhani20@gmail.com>
2026-08-10 17:45:46 +0200

    MQTT + CYBERSECURITY

---
+++ università/Sicurezza delle Reti/MQTT/MQTT/Esercizi/EsercizioChatMQTT/pub_rettore.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mosquitto_pub -h e0d996a0720a4a25ae1a34becc9e8a90.s1.eu.hivemq.cloud -p 8883 -u univr-studenti -P MQTT-esercitazione2026 -t "messaggi/rettore/[RETTORE]" -m "Convocazione senato accademico"
---
+++ università/Sicurezza delle Reti/MQTT/MQTT/Esercizi/EsercizioTemperaturaUmidita/sub_umidita.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mosquitto_sub -h e0d996a0720a4a25ae1a34becc9e8a90.s1.eu.hivemq.cloud -p 8883 -u univr-studenti -P MQTT-esercitazione2026 -t UR -v
